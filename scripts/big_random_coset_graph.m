3a4d1277c8bf056848c7267f81bc84390af985e8
Michael Musty <michaelmusty@gmail.com>
2019-12-05 09:12:37 -0500

    getting towards the end

--- scripts/big_random_coset_graph.m
+++ scripts/big_random_coset_graph.m
@@ -3,7 +3,7 @@ load "config.m";
 SetVerbose("Passport", true);
 
 // user input
-d := 100;
+d := 10;
 
 // code
 sigma0 := Random(Sym(d));
@@ -12,11 +12,11 @@ sigmaoo := (sigma1*sigma0)^-1;
 assert sigmaoo*sigma1*sigma0 eq Id(Sym(d));
 sigma := [sigma0, sigma1, sigmaoo];
 time assert IsTransitive(sub<Sym(d)|sigma>);
-/* time passports := PassportRepresentatives(sigma); */
-/* passport := passports[1]; */
-/* name := Name(sigma); */
+time passports := PassportRepresentatives(sigma);
+passport := passports[1];
+name := Name(sigma);
 
-/* time G, sidepairing := CosetGraph(sigma); */
-/* str := CosetGraphToDotString(G); */
+time G, sidepairing := CosetGraph(sigma);
+str := CosetGraphToDotString(G);
 
-/* WriteCosetGraphToFile(sigma); */
+WriteCosetGraphToFile(sigma);
